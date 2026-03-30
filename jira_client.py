2ef56ebcbef601de25915b8e2fc107c978da73c1
borodinaalena95 <49359721+borodinaalena95@users.noreply.github.com>
2026-03-30 16:51:36 +0200

    add main logic

---
+++ jira_client.py
@@ -0,0 +1,39 @@
+import requests
+import base64
+from config import JIRA_URL, JIRA_EMAIL, JIRA_API_TOKEN
+from requests.auth import HTTPBasicAuth
+
+def get_ticket(ticket_id):
+    url = f"{JIRA_URL}/rest/api/3/issue/{ticket_id}"
+
+    response = requests.get(
+        url,
+        auth=HTTPBasicAuth(JIRA_EMAIL, JIRA_API_TOKEN),
+        headers={"Accept": "application/json"}
+    )
+
+    data = response.json()
+
+    attachments = data["fields"].get("attachment", [])
+    
+    image_base64_strings = []
+    for att in attachments:
+        if att["mimeType"].startswith("image/"):
+            image_url = att["content"]
+            image_response = requests.get(
+                image_url,
+                auth=HTTPBasicAuth(JIRA_EMAIL, JIRA_API_TOKEN)
+            )
+            
+            base64_encoded_image = base64.b64encode(image_response.content).decode('utf-8')
+            
+            # Prepend the data URI scheme
+            mime_type = att["mimeType"]
+            image_base64_strings.append(f"data:{mime_type};base64,{base64_encoded_image}")
+
+
+    return {
+        "title": data["fields"]["summary"],
+        "description": data["fields"]["description"],
+        "images": image_base64_strings
+    }
