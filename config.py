be3a9cce9f0c45f15eb677ca165ce5a085b7cdfb
borodinaalena95 <49359721+borodinaalena95@users.noreply.github.com>
2026-03-31 21:39:27 +0200

    Add JIRA environment variable prints to config.py

--- config.py
+++ config.py
@@ -6,6 +6,10 @@ if not os.getenv('RAILWAY_ENVIRONMENT_ID'):
 
 print("hello" + os.getenv('RAILWAY_ENVIRONMENT_ID'))
 
+print("JIRA_URL:", os.getenv("JIRA_URL"))
+print("JIRA_EMAIL:", os.getenv("JIRA_EMAIL"))
+print("JIRA_API_TOKEN:", os.getenv("JIRA_API_TOKEN"))
+
 JIRA_URL = os.getenv("JIRA_URL")
 JIRA_EMAIL = os.getenv("JIRA_EMAIL")
 JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
